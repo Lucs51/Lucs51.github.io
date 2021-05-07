00f86c05e717e07898eb736a32a0059b2321a373
Lucs51 <32383976+Lucs51@users.noreply.github.com>
2021-05-07 23:37:22 +0200

    commit

---
+++ script.js
@@ -0,0 +1,238 @@
+const team = [
+
+{
+  rank: 1,
+  name: `Niktamer'toa'élarou`,
+  handle: 'Bosh le chiss blanc#4817',
+  img: 'https://cdn.discordapp.com/avatars/331782082277933059/44e12429e80f5c87cec9420aaaa21748.png?size=1024',
+  kudos: '600.000$'},
+  {
+  rank: 2,
+  name: 'Kowi',
+  handle: 'Kowi#0009',
+  img: 'https://cdn.discordapp.com/avatars/296343071866486784/285a8f6d2726784e23759adaba729ba5.png?size=1024',
+  kudos: '500.000$'},
+  {
+  rank: 3,
+  name: 'Sawyer',
+  handle: 'Sawyer#3302',
+  img: 'https://cdn.discordapp.com/avatars/347105684573126678/582bc6bd88a90fe469f0cca9a6d9273c.png?size=1024',
+  kudos: '400.000$'},
+{
+  rank: 4,
+  name: 'Caillou du Banshee',
+  handle: 'Luxoka#6752',
+  img: 'https://cdn.discordapp.com/avatars/267681177849036812/4d1401638164b25f6b66fad89237e5ea.png?size=1024',
+  kudos: '390.000$'},
+{
+  rank: 5,
+  name: 'Zhanshi & Abala',
+  handle: 'Zhanshi#4916',
+  img: 'https://cdn.discordapp.com/avatars/346040604708438017/e61eeb242e7b9ad913dda0c55320d417.png?size=1024',
+  kudos: '300.000$'},
+{
+  rank: 6,
+  name: 'GDZ',
+  handle: 'Luckye#0380',
+  img: 'https://cdn.discordapp.com/avatars/577528852931411978/1f8f94720ec6de73b57ffd756b4ec3ae.png?size=1024',
+  kudos: '200.000$'},
+  {
+  rank: 7,
+  name: 'Vostron',
+  handle: 'Vostron#1329',
+  img: 'https://cdn.discordapp.com/avatars/625306222639185930/3f55b25eab9d9b370eb08029f4b7dcd0.png?size=1024',
+  kudos: '200.000$'},
+  {
+  rank: 8,
+  name: 'Sardoche',
+  handle: '[Lv.30] 𝑀𝒾𝓈𝓈 𝓢𝓴𝓮𝓵𝓮𝓽𝓸𝓷#5818',
+  img: 'https://cdn.discordapp.com/avatars/357184896663420930/2c1daaf6c05a337aa5b9fb8317a42611.webp?size=1024',
+  kudos: '150.000$'},
+{
+  rank: 9,
+  name: 'Alice',
+  handle: '💖𝓞𝓬𝓮𝓪𝓷𝓮💖#0001',
+  img: 'https://cdn.discordapp.com/avatars/670545673262727171/a_0724a70d93399bda78288c0cb8e72a62.png?size=1024',
+  kudos: '77.600$'},
+{
+  rank: 10,
+  name: 'Fargentorm',
+  handle: 'Skolgar#9821',
+  img: `https://cdn.discordapp.com/attachments/697838578948112404/820727798540140544/skol.png`,
+  kudos: '50.000$'},
+{
+ rank: 11,
+ name: 'Gitanix',
+ handle: 'Zermouk#9754',
+  img: 'https://cdn.discordapp.com/avatars/190568822585950209/f06f0d3abe4c0a73c66edecbe8c5711b.png?size=1024',
+  kudos: '40.000$'},
+{
+  rank: 12,
+  name: 'Seigneur Covok',
+  handle: 'Dovak#4492',
+  img: 'https://cdn.discordapp.com/avatars/532558633645375488/2969fc8a62eadaf752fdcf856bf9ffec.png?size=1024',
+   kudos: '30.000$'},
+{
+  rank: 13,
+  name: 'Timo T',
+  handle: 'JayMa バカ#2966',
+  img: 'https://cdn.discordapp.com/avatars/567077625483231232/82e5f19febf78bc9049553efa0028af5.png?size=1024',
+  kudos: '30.000$'},
+{
+  rank: 14,
+  name: 'MisterWeed',
+  handle: 'MisterWeed#7953',
+  img: 'https://cdn.discordapp.com/avatars/394125328882204712/9ca25cbec69826f24022202a997927d2.png?size=1024',
+  kudos: '25.000$'},
+{
+  rank: 15,
+  name: 'Coène',
+  handle: 'ritri#4180',
+  img: 'https://cdn.discordapp.com/avatars/349157443973939201/8d06d6dc9047ed6527ece78d1fbf68f8.png?size=1024',
+  kudos: '24.000$'},
+{
+  rank: 16,
+  name: 'Armada',
+  handle: 'Fantastic Armada#2611',
+  img: 'https://cdn.discordapp.com/avatars/362262678758227978/233d6e49e1481676a1279e378756cc82.png?size=1024',
+  kudos: '20.000$'},
+{
+  rank: 17,
+  name: 'Poivrot',
+  handle: 'Elo-Sama#5296',
+  img: 'https://cdn.discordapp.com/avatars/320362714453245954/434459be0190195fca869def61e34e70.png?size=1024',
+  kudos: '20.000$'},
+{
+  rank: 18,
+  name: 'Abatartufum',
+  handle: '𝑳𝒆𝒌𝒌#6269',
+  img: 'https://cdn.discordapp.com/avatars/295561334332981253/87803008326ed10648b104389273d628.png?size=1024',
+  kudos: '20.000$'},
+{
+  rank: 19,
+  name: 'Bob',
+  handle: 'Le Gnome®#1917',
+  img: 'https://cdn.discordapp.com/avatars/411170233726795778/a_07ea1954a26a9204df684e153f497b3b.png?size=1024',
+  kudos: '12.000$'},
+{
+  rank: 20,
+  name: 'Raic',
+  handle: 'raic_sky#3211',
+  img: 'https://cdn.discordapp.com/avatars/528305889632059394/a2822a1f95b900ad8791c65a5aa6e9da.png?size=1024',
+  kudos: '10.000$'},
+{
+  rank: 21,
+  name: 'DJ Kebo',
+  handle: 'gidor#8217',
+  img: 'https://cdn.discordapp.com/avatars/389153519204302858/9e3868f66b5513b46c7e0036bb0ec7bf.webp?size=1024',
+  kudos: '10.000$'},
+{
+  rank: 22,
+  name: 'Thibault',
+  handle: 'Thibault#3361',
+  img: 'https://cdn.discordapp.com/avatars/179732948826718218/fb12df2e560d9818e6e0218eca06b285.png?size=1024',
+  kudos: '10.000$'},
+{
+  rank: 23,
+  name: 'Hinata',
+  handle: '𝘏 𝘪 𝘯 𝘢 𝘵 𝘢#1947',
+  img: 'https://cdn.discordapp.com/avatars/540311818669785088/1b690039e1cd1eb6a7855c42d15ed35b.png?size=1024',
+  kudos: '8.100$'},
+{
+  rank: 24,
+  name: 'Abdel',
+  handle: 'Maro ツ#2860',
+  img: 'https://cdn.discordapp.com/avatars/497805073540448268/a_0ce90dbdc0f1c912e5aef2e592933190.png?size=1024',
+  kudos: '8.000$'},
+{
+  rank: 25,
+  name: 'Romane',
+  handle: 'Tao et son saké#2155',
+  img: 'https://cdn.discordapp.com/avatars/246659977710796811/c32f2668363753e25fbad651bc24e2f7.png?size=1024',
+  kudos: '8.000$'},
+{
+  rank: 26,
+  name: 'Bosh le pégu',
+  handle: 'Non-précisé',
+  img: 'https://cdn.discordapp.com/attachments/820362180230643726/822474745886015528/unknown.png',
+  kudos: '7.000$'},
+{
+  rank: 27,
+  name: 'Le 69',
+  handle: 'Math⼂#0428',
+  img: 'https://cdn.discordapp.com/avatars/243399544858083329/16be88094573d1c5b9b8635a428b5570.png?size=1024',
+  kudos: '6.942$'},
+{
+  rank: 28,
+  name: 'Tessa',
+  handle: 'Le Gnome®#1917',
+  img: 'https://cdn.discordapp.com/avatars/411170233726795778/a_07ea1954a26a9204df684e153f497b3b.png?size=1024',
+  kudos: '5.000$'},
+{
+  rank: 29,
+  name: 'Karl',
+  handle: 'HugoKEuf#7736',
+  img: 'https://cdn.discordapp.com/avatars/504958762637983756/0b3ea440bad9e613d1c1cf57f208d864.png?size=1024',
+  kudos: '4.000$'},
+  {
+  rank: 30,
+  name: 'Arbiter',
+  handle: 'Non-précisé',
+  img: 'https://static.cnews.fr/sites/default/files/styles/image_640_360/public/point1.jpg?itok=jGTH8XuS',
+  kudos: '4.000$'},
+{
+  rank: 31,
+  name: `jezi'zuka`,
+  handle: '🅲 🅼🅾🅸 🆁🅾🅼🅸#2525',
+  img: 'https://cdn.discordapp.com/avatars/655699755309072385/327cb5c4abc54b4d92b693dd5a91164d.png?size=1024',
+  kudos: '2.000$'},
+{
+  rank: 32,
+  name: 'Skern',
+  handle: 'BlaK_EYED#6302',
+  img: 'https://cdn.discordapp.com/avatars/663759628848463902/723b8333cab8b2ad64c0dc963b051c9d.png?size=1024',
+  kudos: '500$'},
+{
+  rank: 33,
+  name: 'Hooch Gémal',
+  handle: 'shinezeo_siiji#3007',
+  img: 'https://cdn.discordapp.com/avatars/226664730981302272/7c8a6366472eb97d669842a12791cf45.png?size=1024',
+  kudos: '100$'}];
+
+
+team.forEach(member => {
+  let newRow = document.createElement('li');
+  newRow.classList = 'c-list__item';
+  newRow.innerHTML = `
+		<div class="c-list__grid">
+			<div class="c-flag c-place u-bg--transparent">${member.rank}</div>
+			<div class="c-media">
+				<div class="c-media__content">
+					<div class="c-media__title">${member.name}</div>
+					  <a class="c-media__link u-text--small" target="_blank">@${member.handle}</a>
+				</div>
+			</div>
+			<div class="u-text--right c-kudos">
+				<div class="u-mt--8 galbasic">
+					<strong>${member.kudos}</strong>
+				</div>
+			</div>
+		</div>
+	`;
+  if (member.rank === 1) {
+    newRow.querySelector('.c-place').classList.add('u-text--dark');
+    newRow.querySelector('.c-place').classList.add('u-bg--yellow');
+    newRow.querySelector('.c-kudos').classList.add('u-text--yellow');
+  } else if (member.rank === 2) {
+    newRow.querySelector('.c-place').classList.add('u-text--dark');
+    newRow.querySelector('.c-place').classList.add('u-bg--teal');
+    newRow.querySelector('.c-kudos').classList.add('u-text--teal');
+  } else if (member.rank === 3) {
+    newRow.querySelector('.c-place').classList.add('u-text--dark');
+    newRow.querySelector('.c-place').classList.add('u-bg--orange');
+    newRow.querySelector('.c-kudos').classList.add('u-text--orange');
+  }
+  list.appendChild(newRow);
+});
+
+
